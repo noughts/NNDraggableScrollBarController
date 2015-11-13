9597563c4f7c6aa62e8c2f6c7c5977a35e436843
Koichi Yamamoto <noughts@gmail.com>
2015-11-13 15:06:58 +0900

    a

--- Example/NNDraggableScrollBarController/NNViewController.m
+++ Example/NNDraggableScrollBarController/NNViewController.m
@@ -1,29 +1,45 @@
 //
-//  NNViewController.m
-//  NNDraggableScrollBarController
+//  PSViewController.m
+//  PSDraggableScrollBarController
 //
 //  Created by Koichi Yamamoto on 11/13/2015.
 //  Copyright (c) 2015 Koichi Yamamoto. All rights reserved.
 //
 
 #import "NNViewController.h"
+#import <NNDraggableScrollBarController.h>
 
-@interface NNViewController ()
-
-@end
-
-@implementation NNViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+@implementation NNViewController{
+	__weak IBOutlet UITableView* _tableView;
+	NNDraggableScrollBarController* _scrollBarController;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad{
+	[super viewDidLoad];
+	
+	_tableView.contentInset = _tableView.scrollIndicatorInsets;
+	
+	UIImage* knob_img = [UIImage imageNamed:@"grid_scrollBarKnob"];
+	_scrollBarController = [[PSDraggableScrollBarController alloc] initWithScrollView:_tableView knobImage:knob_img];
 }
+
+
+-(void)viewWillAppear:(BOOL)animated{
+	[super viewWillAppear:animated];
+	[_tableView flashScrollIndicators];
+}
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+	return 20;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+	UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+	cell.textLabel.text = [NSString stringWithFormat:@"%@", @(indexPath.row)];
+	return cell;
+}
+
+
 
 @end
